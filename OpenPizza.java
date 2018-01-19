a241af1b21c039edafa13aff772242c79bb21e89
BrianMontenegro12 <brian.montenegro@techhigh.us>
2018-01-19 09:32:44 -0500

    My Inheritance

---
+++ OpenPizza.java
@@ -0,0 +1,12 @@
+
+import java.awt.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class OpenPizza
+{
+    public static void main(String args[])
+    {
+        DrawingPanel draw = new DrawingPanel(1200,1200);
+        Graphics g = draw.getGraphics();
+    }
+}
